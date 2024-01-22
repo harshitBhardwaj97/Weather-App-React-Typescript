537b74e344002dae57a2f42bf4569bedd62205c2
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-22 21:06:00 +0530

    Code Added

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { getWeatherInfo } from "./services/weather-service";
+import WeatherDetails from "./components/WeatherDetails";
+import { type WeatherData } from "../types.ts";
+
+export default function App() {
+  const [location, setLocation] = useState("");
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleEnterKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      fetchWeather(location);
+    }
+  };
+
+  const fetchWeather = async (location: string) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await getWeatherInfo(location);
+      setWeatherData(response);
+    } catch (error) {
+      setError(
+        "Error occured while fetching weather data, make sure correct location is entered!"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="max-w-7xl mx-auto h-screen grid place-content-center bg-gray-200">
+        <main className="p-2 m-2 flex flex-col gap-3 items-center justify-center min-w-[400px] min-h-[420px] border border-black rounded-sm  bg-gray-400">
+          <h1 className="text-center font-bold text-3xl">Weather App</h1>
+          <input
+            type="text"
+            value={location}
+            placeholder="Enter Location"
+            className="px-2 py-4 rounded-full text-xl focus:outline-none shadow-lg border border-black/80 bg-gray-300 text-black/80"
+            onChange={(e) => setLocation(e.target.value)}
+            required
+            onKeyDownCapture={handleEnterKeyPress}
+          />
+
+          {/* To be displayed when no location is entered */}
+          {!location && (
+            <div className="text-center p-2 text-red-700 font-bold">
+              Enter valid location to get weather details
+            </div>
+          )}
+
+          {/* Only show button when location is entered */}
+          {location && (
+            <button
+              className="font-bold text-2xl px-4 py-2 rounded-3xl bg-gray-500 mx-auto w-[50%]
+            hover:bg-red-500 hover:text-white ease-linear duration-150
+            "
+              onClick={() => fetchWeather(location)}
+            >
+              Get Weather
+            </button>
+          )}
+
+          {/* While request is not resolved*/}
+          {loading && (
+            <div className="font-bold text-blue-800 underline">
+              Loading Weather Details ...
+            </div>
+          )}
+
+          {/* In case of Error */}
+          {error && (
+            <div className="text-red-700 font-bold w-[70%]">{error}</div>
+          )}
+
+          {/* Request Fetched Successfully, render the weather data */}
+          {weatherData && !loading && !error && (
+            <WeatherDetails {...weatherData} />
+          )}
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ src/components/WeatherDetails.tsx
@@ -0,0 +1,46 @@
+import { type WeatherData } from "../../types.ts";
+
+export default function WeatherDetails(props: WeatherData) {
+  const { main, name, sys, weather, wind } = props;
+
+  return (
+    <div className="flex items-center gap-2 p-2 relative bg-gray-300 rounded-md shadow-xl min-w-[360px]">
+      <div className="flex-1 flex flex-col items-center justify-evenly h-full gap-2">
+        {/* Location Details */}
+        <h2>
+          <span className="font-bold text-xl">{name}</span>, {sys.country}{" "}
+          <br />
+          {weather[0].description}
+        </h2>
+        <div>
+          <p>
+            {/* Temparature */}
+            <span className="font-bold text-3xl">{main.temp} °C</span>
+          </p>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-evenly gap-2">
+        {/* Weather Icon */}
+        <img
+          className="rounded-lg"
+          src={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+          alt="Weather Icon"
+        />
+        <div className="flex flex-col items-center text-sm">
+          <p>
+            <span className="italic">Feels Like</span> :{" "}
+            <span className="font-bold">{main.feels_like} °C</span>{" "}
+          </p>
+          <p>
+            <span className="italic">Humidity</span> :{" "}
+            <span className="font-bold">{main.humidity}%</span>{" "}
+          </p>
+          <p>
+            <span className="italic">Wind Speed</span> :{" "}
+            <span className="font-bold">{wind.speed} m/s</span>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
