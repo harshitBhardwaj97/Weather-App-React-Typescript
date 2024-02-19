4de0b51c7718c237b43dd7e272d0327e200c3e44
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-02-19 19:21:22 +0530

    global timeout set as 10 seconds, #skip deployment

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     },
     baseUrl: "http://localhost:5173",
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
+    defaultCommandTimeout: 10000, // Increased global timeout to 10 seconds
   },
   projectId: "t6srp4",
 });
